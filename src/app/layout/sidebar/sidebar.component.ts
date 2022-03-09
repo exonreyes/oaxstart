d73e6fc6ae1147f28ed904f2cbb19e6e767e0ab6
exon-reyes <exon1704@gmail.com>
2022-03-09 10:24:41 -0600

    Creando menu de modulos

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -2,13 +2,17 @@ import {Component, OnInit} from '@angular/core';
 import {SideBarService} from "@data/services/api/SideBarService";
 import {faMicrochip} from "@fortawesome/free-solid-svg-icons/faMicrochip";
 import {faLaptopMedical} from "@fortawesome/free-solid-svg-icons/faLaptopMedical";
+import {faBed} from "@fortawesome/free-solid-svg-icons/faBed";
+import {faHospitalUser} from "@fortawesome/free-solid-svg-icons/faHospitalUser";
+import {faBusinessTime} from "@fortawesome/free-solid-svg-icons/faBusinessTime";
 
 @Component({
   selector: 'app-sidebar', templateUrl: './sidebar.component.html', styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
-  iApps = faMicrochip;
+  iColaborator = faHospitalUser;
   iMedical = faLaptopMedical;
+  iBusiness=faBusinessTime;
 
   constructor(private sideBarService: SideBarService) {
 
